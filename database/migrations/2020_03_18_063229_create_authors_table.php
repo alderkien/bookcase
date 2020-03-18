af84762db5d83aaed54e43339bab71514a5eb685
alderkien <realderkien@gmail.com>
2020-03-18 14:06:56 +0700

    add models book, author

---
+++ database/migrations/2020_03_18_063229_create_authors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('surname',200);
+            $table->string('name',200);
+            $table->string('patronymic',200);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
