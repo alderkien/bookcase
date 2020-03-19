62caa7d7f569c975fb2b4cc18f2ae18c8324384a
alderkien <realderkien@gmail.com>
2020-03-19 15:03:14 +0700

    Tests Books and Authors

---
+++ tests/Feature/AuthorTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthorTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    use RefreshDatabase;
+    
+    public function testAuthors()
+    {
+        $response = $this->get('/authors');
+        $response->assertStatus(200);
+    }
+}
---
+++ tests/Feature/BookTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+
+class BookTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    use RefreshDatabase;
+
+    public function testBooks()
+    {
+        $response = $this->get('/books');
+        $response->assertStatus(200);
+    }
+
+    public function testCreateAccess()
+    {
+        $response = $this->get('/books/create');
+        $response->assertStatus(302);
+    }
+
+    public function testStoreAccess()
+    {
+        $response = $this->get('/books/store');
+        $response->assertStatus(405);
+
+        $response = $this->post('/books/store');
+        $response->assertStatus(302);
+    }
+
+}
